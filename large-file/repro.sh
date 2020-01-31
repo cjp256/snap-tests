ed37e309d47e88a9966cf788f7a418c65321fef2
Chris Patterson <chris.patterson@canonical.com>
2020-01-31 10:21:43 -0500

    add large-file test

---
+++ large-file/repro.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e -x
+
+sudo snap install multipass --classic || true
+sudo snap install snapcraft --classic || true
+
+if [ ! -f data/large-file ]; then
+  mkdir -p data
+  dd if=/dev/zero of=data/large-file bs=1M count=10000
+fi
+
+export SNAPCRAFT_BUILD_ENVIRONMENT_DISK="64G"
+export SNAPCRAFT_BUILD_ENVIRONMENT"=multipass"
+
+while true; do
+  for i in $(seq 1 1200); do
+    sleep $i
+    snapcraft
+  done
+done
